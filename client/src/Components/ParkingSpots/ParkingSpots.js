a35d7f421ad6d900a5dc1ceffb74852369097c57
GabrielIvanG <gabriel.ivan.gonzalez@gmail.com>
2021-07-23 21:14:50 -0300

    improving responsiveness

--- client/src/Components/ParkingSpots/ParkingSpots.js
+++ client/src/Components/ParkingSpots/ParkingSpots.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ParkingSpots.css'
 import findIcon from '../../icons/findIcon.png'
 import Spots from './Spots/Spots.js'
-
-var axios = require('axios');
-var data = '';
-
-var config = {
-  method: 'get',
-  url: 'https://api.yelp.com/v3/businesses/search?term=parking-lot&location=90001',
-  headers: { 
-    'Authorization': 'Bearer 8wWR72TAtg6J5IxB35oKOXRjpbAN717dVWGY8qlRPM9fnao7wYo0JEsUjcl20vwJzKRHz5XSicGywNreXZZeZ1k2LiOwISf-hVCCZhpHaQOc2SEIDtun_tRl-dv1YHYx'
-  },
-  data : data
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+const {BACKEND_URL} = process.env
 
 
 
 export default function ParkingSpots() {
+
+  const [location, setLocation] = useState([])
+
+  const formSubmit = (e) => {
+    alert(location)
+  }
+
     return (
         <>
-        <div className='ParkingSpots'>
-            <input placeholder='address,neighborhood,city,state or zip...'></input>
-            <img src={findIcon} id='findButton' alt='findIcon' />
-        </div>
+        <form onSubmit={formSubmit} className='ParkingSpots'>
+            <input placeholder='address,neighborhood,city,state or zip...'
+                  onChange={(e)=> {setLocation(e.target.value)}}
+            />
+            <button>
+              <img src={findIcon} id='findButton' alt='findIcon' />
+            </button>
+        </form>
         <Spots />
         </>
     )
